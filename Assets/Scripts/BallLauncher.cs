841b76fb13562e1d93364535dbfdbedd81310851
MartinGebske <martin.gebske@qmarketing.de>
2016-11-18 22:09:45 +0100

    initial commit

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+	public GameObject ball;
+    public float ballSpeed = 8.0f;
+	public static bool ballLives;
+	Camera cam;
+
+
+    // Use this for initialization
+    void Start () 
+	{
+		cam = GetComponentInChildren<Camera>();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)&& !ballLives)
+        {
+            GameObject instance = Instantiate(ball);
+            instance.transform.position = transform.position;
+            
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = cam.transform.rotation * Vector3.forward * ballSpeed;
+			ballLives = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider col)
+	{
+		Destroy (col.gameObject);
+		BallLauncher.ballLives = false;
+	}
+
+}
