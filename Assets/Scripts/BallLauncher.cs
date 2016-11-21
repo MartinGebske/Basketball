a147aea9f5476d0f97e6e94b9f32cd58963ab948
Martin Gebske <martin.gebske@hotmail.de>
2016-11-21 08:21:11 +0100

    Ball gets parented to Player properly. Goes into Grabbing / Shooting State. … but doesn’t shoot yet ^

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -3,54 +3,67 @@ using System.Collections;
 
 public class BallLauncher : MonoBehaviour {
 
+	public enum States { GRABBING, SHOOTING }
 
     public float ballSpeed = 8.0f;
 
 	public float windowOfOportunity = 2F;
+
+	public Transform grabPosition;
 
 	private Camera cam;
 
 	private Shredder shredder;
 	private Ball ball;
 
+	States StateMachine;
+
+	States ShootState = States.SHOOTING;
+	States GrabState = States.GRABBING;
+
     void Start () 
 	{
-		cam = GetComponent<Camera>();
+		
+		cam = GetComponentInChildren<Camera>();
 		shredder = FindObjectOfType<Shredder> ();
 	}
-	
+
 
 	void Update () 
 	{
 		ball = GameObject.FindObjectOfType<Ball> ();
 
-		windowOfOportunity -= Time.deltaTime;
-	
+		if (StateMachine == ShootState) {
+			windowOfOportunity -= Time.deltaTime;
+			if (windowOfOportunity <= 0) {
+				ShootBall ();
+			}	
+		}
 
 		if (windowOfOportunity < -10 && ball == null) {
 			shredder.DestroyBall ();
 			windowOfOportunity = 2;
 		}
-			
-		
     }
 
 	public void GrabBall()
 	{
-		ball.transform.position = transform.position;
+		ball.transform.position = grabPosition.transform.position;
 		Rigidbody rb = ball.GetComponent<Rigidbody>();
 		rb.useGravity = false;
-		//if (windowOfOportunity <= 0 && ball != null)
-		//{
-		//	ShootBall ();
-		//	windowOfOportunity = 2;
-		//}
+		ball.transform.SetParent (grabPosition);
+		StateMachine = ShootState;
 	}
 
 
 	void ShootBall()
 	{
-		Rigidbody rb = ball.GetComponent<Rigidbody>();
-		rb.velocity = cam.transform.rotation * Vector3.forward * ballSpeed;
+			Rigidbody rb = ball.GetComponent<Rigidbody>();
+			rb.useGravity = true;
+			rb.velocity = cam.transform.rotation * Vector3.forward * ballSpeed;
+	}
+	void ResetBall()
+	{
+		
 	}
 }
